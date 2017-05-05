d95a16dd77609ac589468b1b34d63600c1ea0a7c
TreeNode <ashot.seropyan@gmail.com>
2017-05-05 21:13:15 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import P5 from 'p5';
-import Walker from './walker';
 
 
+let t1 = 0;
+let t2 = 108;
 const sketch = function (p) {
-    let walker = new Walker(p);
     p.setup = function () {
         p.createCanvas(640, 480);
-        walker.init();
     };
 
     p.draw = function () {
-        walker.update();
-        walker.draw();
+        let n1 = p.noise(t1);
+        let n2 = p.noise(t2);
+        let x = p.map(n1, 0, 1, 0, p.width);
+        let y = p.map(n2, 0, 1, 0, p.height);
+        p.ellipse(x, y, 16, 16);
+        t1 += 0.01;
+        t2 += 0.01;
     };
 };
 
