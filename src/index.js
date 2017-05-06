e21dc89e2e21c5209f0d5e7a26dbfbaf082777fb
TreeNode <ashot.seropyan@gmail.com>
2017-05-06 16:52:12 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import P5 from 'p5';
-import Ball from './ball';
 
 
 const sketch = function (p) {
-    let ball = new Ball(p);
     p.setup = function () {
         p.createCanvas(640, 480);
-        ball.init();
     };
 
     p.draw = function () {
-        p.background(255);
-        ball.update();
-        ball.draw();
+        p.background(200);
+
+        let mouse = p.createVector(p.mouseX, p.mouseY);
+        let center = p.createVector(p.width/2, p.height/2);
+
+        mouse.sub(center);
+        mouse.setMag(50);
+
+        p.translate(center.x, center.y);
+
+        p.fill(100);
+        p.ellipse(0, 0, 100, 100);
+
+        p.stroke(255);
+        p.line(0, 0, mouse.x, mouse.y);
     };
 };
 
